0eca71a4b8e33887e56d30c800059dd9c21512b9
Ravindra Babu Danduboyina <81628668+Ravindrababu87@users.noreply.github.com>
2021-06-05 21:07:20 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,43 @@
+package employee;
+
+public class Employee
+{
+	private String name;
+  
+	private String address;
+  
+	private String mobileno;
+	
+	public String getName() {
+    
+		return name;
+    
+	}
+	public void setName(String name) {
+    
+		this.name = name;
+    
+	}
+	public String getAddress() {
+    
+		return address;
+	}
+	public void setAddress(String address) {
+    
+		this.address = address;
+	}
+  
+	public String getMobileno() {
+    
+		return mobile;
+    
+	}
+	public void setMobileno(String mobileno) {
+    
+		this.mobileno = mobileno;
+	}
+
+	
+	
+
+}
